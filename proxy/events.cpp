96ae8aa1a3e9801ae1cdafc22ceb8deb57b22fbe
carryKindoff <80630834+carryKindoff@users.noreply.github.com>
2021-04-06 22:14:09 +0300

    Update events.cpp
    Added Report player

--- proxy/events.cpp
+++ proxy/events.cpp
@@ -73,7 +73,9 @@ bool events::out::generictext(std::string packet) {
                 if (mode.find("add") != -1) {
                     g_server->send(false, "action|dialog_return\ndialog_name|popup\nnetID|" + motion + "|\nnetID|" + motion + "|\nbuttonClicked|friend_add");
                 }
-                return true;
+                if (mode.find("report") != -1) {
+                    g_server->send(false, "action|dialog_return\ndiaolg_name|popul\nnetID|" + motion + "|\nnetID" + motion + "|\nbuttonClicked|report_player"):
+		return true;
             }
         }
     if (var.get(0).m_key == "action" && var.get(0).m_value == "input") {
